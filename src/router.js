02459e6bb46dab4295aa38f704ccc1be38ee9afb
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-11-13 19:31:03 +0100

    feat(router) Implement Router For Single Thread Show :beers:

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// The vue is given a name, here Home
 import Home from './views/Home.vue'
+import ThreadShow from '@/components/ThreadShow'
 
 Vue.use(Router)
 
@@ -20,6 +22,15 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      // Dynamic segment need to build our URL
+      path: '/thread/:id',
+      // Always use the CRUD verb when naming the component
+      name: 'ThreadShow',
+      component: ThreadShow,
+      // Setting props to true will send params to the child automatically
+      props: true
     }
   ]
 })
