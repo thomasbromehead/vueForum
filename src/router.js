dc69d3fce3533f5311c44dcc39f1cee63250c20e
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-11-15 20:11:27 +0100

    Add NotFound * path and redirect link to Homepage

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 // The vue is given a name, here Home
 import ThreadShow from '@/pages/PageThreadShow'
 import Home from '@/pages/PageHome'
+import NotFound from '@/pages/PageNotFound'
 
 Vue.use(Router)
 
@@ -25,12 +26,17 @@ export default new Router({
     },
     {
       // Dynamic segment need to build our URL
-      path: '/thread/show/:id',
+      path: '/thread/:id',
       // Always use the CRUD verb when naming the component
       name: 'ThreadShow',
       component: ThreadShow,
       // Setting props to true will send params to the child automatically
       props: true
+    },
+    {
+      path: '*',
+      name: "NotFound",
+      component: NotFound
     }
   ]
 })
